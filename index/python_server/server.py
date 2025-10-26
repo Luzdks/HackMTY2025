1421a7d5259e7fda785b079b10cd1b23ea222446
Ángel Darío Marroquín Escobedo <angeldariux@gmail.com>
2025-10-26 05:04:23 -0600

    Update server.py

--- index/python_server/server.py
+++ index/python_server/server.py
@@ -1,7 +1,8 @@
 # server.py (para conectar Python con Flutter)
 from flask import Flask, jsonify
 from flask_cors import CORS
-from index.python_server.noticias_gemini import get_positive_news
+from noticias_opt import get_positive_news
+
 
 app = Flask(__name__)
 CORS(app)  # Esto permite que Flutter se conecte
@@ -9,10 +10,12 @@ CORS(app)  # Esto permite que Flutter se conecte
 @app.route('/api/news', methods=['GET'])
 def get_news():
     try:
-        news = get_positive_news()
-        return jsonify(news)
+        result = get_positive_news()
+        return jsonify(result)
     except Exception as e:
+        print("❌ Error en /api/news:", e)
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
+    # inicia servidor Flask accesible en tu red local
     app.run(host='0.0.0.0', port=5000, debug=True)
